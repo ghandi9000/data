aedcfc5e8da59c995050f4c00570cbce59922bd8
Noah Peart <noah.v.peart@gmail.com>
2013-11-02 18:31:45 -0400

    Batch update

--- data-trans/hazards/si-sdp-time-classes.R
+++ data-trans/hazards/si-sdp-time-classes.R
@@ -16,3 +16,15 @@ bc$si.hazard <- as.numeric(factor(bc$si.hazard))
 remove <- c("sdp1.hazard")
 bc <- bc[,!names(bc) %in% remove]
 write.csv(bc, "~/work/data/data/hazards/hazards-bc-firs.csv", row.names=FALSE)
+
+## create a column to determine death / hazard period (0 didn't die, 1 did die)
+bc$died <- rep(0, nrow(bc))
+yrs <- c(85, 91, 97)
+periods <- c(4,5,6)
+for (i in 1:length(yrs)) {
+    print(periods[i])
+    col <- paste0("died",yrs[i])
+    bc[bc$time == periods[i],"died"] <- bc[bc$time == periods[i], col]
+}
+
+nrow(bc[bc$time == periods[i],]$died)
